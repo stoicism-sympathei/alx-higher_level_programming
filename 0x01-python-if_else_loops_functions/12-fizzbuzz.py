f45095045e5539726d8577a28f72272b7a46c939
stoicism-sympathei <mogesanonymous@gmail.com>
2022-11-29 17:08:07 +0300

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100 + 1):
-        if i % 3 == 0 or i % 5 == 0:
-            if i % 3 == 0:
-                print("Fizz", end='')
-            if i % 5 == 0:
-                print("Buzz", end='')
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
