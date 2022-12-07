8b7b513dd8f0b29e2a67c95f5c2ea992d36d762e
stoicism-sympathei <mogesanonymous@gmail.com>
2022-12-07 12:03:40 +0300

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]);
-return [[y ** 2 for y in x] for x in matrix]
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
