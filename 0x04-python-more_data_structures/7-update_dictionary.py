9ca2aa95469cd6c5697d98ab53f046bbce1a5664
stoicism-sympathei <mogesanonymous@gmail.com>
2022-12-07 13:33:41 +0300

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key , value):
+def update_dictionary(a_dictionary, key, value):
     if key not in a_dictionary:
         a_dictionary[key] = value
     else:
