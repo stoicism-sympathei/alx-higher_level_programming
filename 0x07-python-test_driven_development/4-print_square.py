54d150d05c8c92bb2fff1f4d20cd1773dd84a5f5
stoicism-sympathei <mogesanonymous@gmail.com>
2023-01-04 16:55:35 +0300

    Text indentation

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""
-This is the print_square module.
-This module supplies one function, print_square().
-"""
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """
-    Print a square with the character #.
+    """Print a square with the # character.
     Args:
-        size (int): the size length of the square.
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
-    if type(size) is not int:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    print(("#" * size + '\n') * size, end='')
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
+
