948970e1730e406b483aab0dbf5f59d277e30779
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-14 20:47:44 +0300

    coreection

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-const fileA = fs.readFileSync(process.argv[2], 'utf8');
-const fileB = fs.readFileSync(process.argv[3], 'utf8');
-
-fs.writeFileSync(process.argv[4], fileA + fileB);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
