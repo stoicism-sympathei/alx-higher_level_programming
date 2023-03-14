8efcd1f4ee9a0f8930d315793a7918b8c08010e7
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-14 20:36:17 +0300

    fixed

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
-const output = {};
 
-for (const entry in dict) {
-  if (!output[dict[entry]]) output[dict[entry]] = [];
-  output[dict[entry]].push(entry);
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
+  }
+  newDict[valsUniq[j]] = list;
 }
-
-console.log(output);
+console.log(newDict);
