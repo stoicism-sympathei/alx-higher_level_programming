54bda9bf19141da66f4822939f13d31349488be6
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-31 13:19:16 +0300

    Error Code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL
-and displays the body of the response (decoded in utf-8)."""
+
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
+"""
 
 from urllib import request, error
 from sys import argv
