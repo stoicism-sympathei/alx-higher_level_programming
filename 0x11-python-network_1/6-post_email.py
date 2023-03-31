a6731649d75b4e8af802314430e08429c5a82029
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-31 14:10:30 +0300

    POST an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a
-parameter, and finally displays the body of the response."""
-
+""" post email """
 import requests
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    print(requests.post(argv[1], data={'email': argv[2]}).text)
+    email = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=email)
+    print(r.text)
