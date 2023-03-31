83f1dfc9c09ea94f6d805e42bda040b65fd7fb0d
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-31 14:03:03 +0300

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""lists the 10 most recent commits on a given GitHub repository.
-"""
-import sys
-import requests
+"""Python script that lists list 10 commits (from the most recent
+to oldest) of the repository “rails” by the user “rails”."""
 
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+          .format(argv[2], argv[1])
+
+    request = requests.get(url)
+    commits = request.json()
+    for commit in commits:
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit").get("author").get("name")))
