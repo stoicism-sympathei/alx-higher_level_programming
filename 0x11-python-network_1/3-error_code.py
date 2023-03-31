55519e4c9819edb78df79fab8cd92da04f65c0fe
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-31 13:21:50 +0300

    Error Code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,12 +6,12 @@
 - displays the body of the response (decoded in utf-8).
 """
 
-from urllib import request, error
-from sys import argv
-
 if __name__ == "__main__":
-        try:
-            with request.urlopen(argv[1]) as page:
-                print(page.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
